6394c876f1dd2b53dee14b8e15d12ac8835a5ee4
云云 <52269948+ngc660sec@users.noreply.github.com>
2025-02-12 10:06:54 +0800

    Update ConfigServer.py

--- Config/ConfigServer.py
+++ Config/ConfigServer.py
@@ -36,27 +36,6 @@ def returnFingerConfigData():
     return configData
 
 
-def returnFeishuConfigData():
-    """
-    返回飞书配置文件数据
-    :return:
-    """
-    current_path = returnConfigPath()
-    configData = yaml.load(open(current_path + '/Feishu.yaml', mode='r', encoding='UTF-8'), yaml.Loader)
-    return configData
-
-
-def saveFeishuConfigData(configData):
-    """
-    保存飞书配置
-    :param configData:
-    :return:
-    """
-    current_path = returnConfigPath()
-    with open(current_path + '/Feishu.yaml', mode='w') as file:
-        yaml.dump(configData, file)
-
-
 def returnRoomMsgDbPath():
     return returnConfigPath() + 'RoomMsg.db'
 
